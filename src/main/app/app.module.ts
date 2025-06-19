9d1331a4bbcebc7837ea9e95314ef49d0ca24e80
efficier <k37844350@gmail.com>
2025-06-20 00:45:56 +0900

    preload 위치 수정

--- src/main/app/app.module.ts
+++ src/main/app/app.module.ts
@@ -22,7 +22,7 @@ import { PrismaService } from './shared/prisma.service'
           autoHideMenuBar: true,
           webPreferences: {
             contextIsolation: true,
-            preload: join(__dirname, '../preload/index.js'),
+            preload: join(__dirname, '../preload/index.cjs'),
           },
         })
 
