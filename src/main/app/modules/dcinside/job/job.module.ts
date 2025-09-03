80d4e3593d2ea98bff80a0010d49c69ec16a50d2
efficier <k37844350@gmail.com>
2025-09-04 01:13:40 +0900

    스케줄링 두번실행되는 버그 수정

--- src/main/app/modules/dcinside/job/job.module.ts
+++ src/main/app/modules/dcinside/job/job.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { JobQueueProcessor } from './job-queue.processor'
 import { JobController } from './job.controller'
-import { ScheduleModule } from '@nestjs/schedule'
 import { CommonModule } from '@main/app/modules/common/common.module'
 import { PostJobModule } from '@main/app/modules/dcinside/post-job/post-job.module'
 import { JobLogsModule } from '@main/app/modules/dcinside/job-logs/job-logs.module'
@@ -9,7 +8,7 @@ import { SettingsModule } from '@main/app/modules/settings/settings.module'
 import { DcinsideApiModule } from '@main/app/modules/dcinside/api/api.module'
 
 @Module({
-  imports: [ScheduleModule.forRoot(), CommonModule, PostJobModule, JobLogsModule, SettingsModule, DcinsideApiModule],
+  imports: [CommonModule, PostJobModule, JobLogsModule, SettingsModule, DcinsideApiModule],
   providers: [JobQueueProcessor],
   controllers: [JobController],
   exports: [JobQueueProcessor],
